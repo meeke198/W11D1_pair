d2c886a86f9f336200e68f93a72b1913d14e1ebb
hien nguyen <thuhiennguyen198@gmail.com>
2021-10-18 12:17:01 -0400

    render Game

--- components/game.jsx
+++ components/game.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import * as Minesweeper from "../minesweeper"
 class Game extends React.Component {
   constructor(props) {
+      console.log("this is props", props);
     super(props);
     this.state = {
       board: new Minesweeper.Board(9, 9),
     };
+    this.updateGame = this.updateGame.bind(this);
   }
   updateGame(){
 
@@ -13,8 +15,11 @@ class Game extends React.Component {
   render(){
       return(
           <div>
-
+              <Board board = {this.state.board} update = {this.updateGame} />
+              <p>{console.log("inside render", this)}</p>
+            {/* <p>react is working</p> */}
           </div>
       )
   }
 }
+export default Game;
---
+++ entry.jsx
@@ -0,0 +1,9 @@
+import Game from "./components/game";
+import React from "react"; //include React
+import ReactDOM from "react-dom"; //give access to ReactDOM
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const root = document.getElementById("root");
+  ReactDOM.render(<Game />, root);
+});
